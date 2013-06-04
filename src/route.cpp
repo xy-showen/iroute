5d5697b110d3a298a2c2f4422d64bf135906b680
snoopyxdy <snoopyxdy@gmail.com>
2013-06-04 23:44:56 +0800

    test close

--- src/route.cpp
+++ src/route.cpp
@@ -183,7 +183,8 @@ void route::worker_callback(Request &req){
 
 
 	char *char_uri = strtok(req.url, &sign1);
-
+	char *param = strtok(NULL, &sign1);
+	
 	handler_route* handler_p = 0;
 
 
@@ -210,7 +211,7 @@ void route::worker_callback(Request &req){
 
 
 
-	char *param = strtok(NULL, &sign1);
+	
 
 	std::cout<<param<<std::endl;
 	std::cout<<"~~~~~~~~~~~uri~~~~~~~~~~"<<std::endl;
